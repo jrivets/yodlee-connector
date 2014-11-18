b32c8294a8e20be085de21e7406e135aeb10f200
kbabushkin <kbabushkin@mulesoft.com>
2014-11-18 00:51:53 -0800

    project skeleton

--- src/main/java/org/jrivets/connector/yodlee/Constants.java
+++ src/main/java/org/jrivets/connector/yodlee/Constants.java
@@ -10,9 +10,4 @@ final class Constants {
     static final String COBRAND_LOGIN_PATH = "/authenticate/coblogin";
     static final String USER_LOGIN_PATH = "/authenticate/login";
 
-    static final String COBRAND_LOGIN_PARAM = "cobrandLogin";
-    static final String COBRAND_PASSWD_PARAM = "cobrandPassword";
-
-    static final String USER_LOGIN_PARAM = "login";
-    static final String USER_PASSWD_PARAM = "password";
 }
